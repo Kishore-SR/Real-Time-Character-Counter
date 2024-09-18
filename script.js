e8bf55b8fb2fb35bf37306e2b0adc934a6bbab1b
Kishore S R <kishoresr01@gmail.com>
2024-09-17 21:09:49 -0400

    Updated code

--- script.js
+++ script.js
@@ -10,3 +10,39 @@ function updateTimeDate() {
 setInterval(updateTimeDate, 1000); // Time is updated every second
 updateTimeDate(); // Initial Function call
 
+// FUNCTION TO TRACK THE CHARACTER COUNT
+let totalCount = document.getElementById('total');
+let remainingCount = document.getElementById('remain');
+let textArea = document.getElementById('text-container');
+let btn =  document.querySelector('.tweet-btn');
+
+textArea.addEventListener("input", ()=> {
+    updateCounter();
+});
+
+function updateCounter(){
+    const charCount = textArea.value.length;
+    totalCount.innerText = charCount;
+    remainingCount.innerText = 100 - charCount;
+
+    // Enable 'TWEET' button if character count is > 0 and < 100
+    if(charCount > 0 && charCount <= 100){
+        btn.classList.add('js-active');
+        btn.classList.remove('js-disable');
+        btn.setAttribute('href', 'https://x.com/Kishore-SR'); // Enable the link
+    }
+    else {
+        btn.classList.add('js-disable');
+        btn.classList.remove('js-active');
+        btn.removeAttribute('href'); // Link gets disabled
+    }
+
+    // If character count exceeds 100 chars
+    if(charCount > 100) {
+        textArea.style.backgroundColor = "#f8d7da"; // Textare to 'Red colour
+    } else {
+        textArea.style.backgroundColor = "#fcfcfc"; // Default color
+    }
+}
+
+updateCounter(); // INITIAL FUNCTION CALL
